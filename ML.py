2013477a23cf793204c9d8fe00ee120ac7bc4f7c
Senfidel <137909173+Senfidel@users.noreply.github.com>
2024-06-04 23:19:39 +0200

    Update ML.py

--- ML.py
+++ ML.py
@@ -1,7 +1,7 @@
 #Librairies de base
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 #Librairies pour le machine learning
